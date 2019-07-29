55fa05a05e5be15bbbbcec97916a379738be1086
ladref <louis.dreyfus@icloud.com>
2019-07-29 16:28:57 +0200

    everything

--- lib/controller.rb
+++ lib/controller.rb
@@ -3,7 +3,7 @@ require 'gossip'
 class ApplicationController < Sinatra::Base
 
   get '/' do
-    erb :index
+    erb :index, locals: {gossips: Gossip.all}
   end
 
   get '/gossips/new/' do
@@ -11,10 +11,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/gossips/new/' do
-    erb :gossip
-    Gossip.new.save
+    puts "Salut, je suis dans le serveur"
+    puts "Ceci est le contenu du hash params : #{params}"
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    puts"Vous allez être redirigé au menu principal."
+    redirect '/'
   end
-
+  #Params est une variable qui permet de récupérer les éléments du formulaire "new_gossip.erb"
 
 
 # End of the ApplicationController class
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -2,16 +2,26 @@ require 'pry'
 require 'csv'
 
 class Gossip
+  attr_accessor :author, :content
+
+  def initialize(author, content)
+    @author = author
+    @content = content
+  end
 
   def save
-    CSV.open("../db/gossip.csv", "ab") do |csv|
-  csv << ["Mon super auteur", "Ma super description"]
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+  csv << [@author, @content]
     end
   end
 
-
-
-
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |row|
+      all_gossips << Gossip.new(row[0], row[1])
+    end
+    return all_gossips
+  end
 
 
 # End of the Gossip class
